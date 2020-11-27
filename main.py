24d7fd7551e9bfd8c8314a865be09541d0162c4f
Ushwald <oscartanis@gmail.com>
2020-11-27 18:08:16 +0100

    Midi functionality is work in progress, but I figured out how to play music from within python which should make life easier

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import midi
 import mido
 from midiutil import MIDIFile
 from markov_chain_model import generate_markov_chain
+from midiplayer import *
 
 def getTextFromMidi(file):
     mid = mido.MidiFile(file)
@@ -36,8 +37,7 @@ class Composition():
             for sixteenth_idx, pitch in enumerate(voice):
                 if voice[sixteenth_idx] == 0:
                     continue
-                MyMIDI.addNote(track, voice_idx, voice[sixteenth_idx], sixteenth_idx, duration, volume)
-
+                MyMIDI.addNote(track, voice_idx, int(voice[sixteenth_idx]), sixteenth_idx, duration, volume)
 
         with open(filename, "wb") as output_file:
             MyMIDI.writeFile(output_file)
@@ -45,14 +45,38 @@ class Composition():
     
 
 
-def load_music():
-    F = np.loadtxt("sample/F.txt")
+def load_music_txt(filename = "sample/F.txt"):
+    # Reads music from a txt file
+    F = np.loadtxt(filename)
     return F
+
+def load_music_midi(file):
+    # Reads midi file, outputs a rawdata format of music (like F.txt)
+    #TODO WIP
+    mid = mido.MidiFile(file)
+    # Summary of midi file
+    print(mid)
+
+    # Find out how many channels there are
+    channels = [False for i in range(16)]
+    print("Loading file: {}...".format(file))
+    for msg in mid.play():
+        print(msg)
+        if not channels[msg.channel]:
+            channels[msg.channel] = True
+        
+    
+    print(channels)
+    print(np.sum(channels))
 
 
 def main():
+    # First off: some configuration:
+    m_F = "sample/F.txt"
+    m_output = "output/BachFromTheDead.mid"
+
     # Load the training data
-    music_file = load_music()
+    music_file = load_music_txt()
 
     # TODO: Transform the data into something useful for the ML algorithm
 
@@ -61,12 +85,26 @@ def main():
 
     # Generate the new music
     generated_music = np.array([mc[idx].generate_states(music_file[0, idx], no=200) for idx in range(4)]).T
-
+    
     # Transform the composition to Midi and write it to a file
     C = Composition(generated_music)
-    C.writeMIDI("BachFromTheDead.mid")
+    C.writeMIDI(m_output)
 
-    getTextFromMidi("BachFromTheDead.mid")
+    #TODO: WIP function; not yet functional
+    #load_music_midi("BachFromTheDead.mid")
+
+
+    # Finally; play the generated music file
+    midi_filename = m_output
+    try:
+        # use the midi file you just saved
+        play_music(midi_filename)
+    except KeyboardInterrupt:
+        # if user hits Ctrl/C then exit
+        # (works only in console mode)
+        pygame.mixer.music.fadeout(1000)
+        pygame.mixer.music.stop()
+    raise SystemExit
 
 if __name__=="__main__":
     main()
