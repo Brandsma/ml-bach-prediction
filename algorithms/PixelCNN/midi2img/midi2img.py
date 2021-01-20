ad06d3c21fc9ce58d937de17ccbd527c5721cfd8
IvoSte <36919251+IvoSte@users.noreply.github.com>
2021-01-20 16:21:58 +0100

    midi2images splits music in parts and generates the correct amount of images. Added the function midi2binary that gives a df with the images as binary matrices. Please do not look at the code, we are deeply ashamed

--- algorithms/PixelCNN/midi2img/midi2img.py
+++ algorithms/PixelCNN/midi2img/midi2img.py
@@ -4,6 +4,7 @@ import json
 import sys
 import numpy as np
 from imageio import imwrite
+import math
 
 def extractNote(element):
     return int(element.pitch.ps)
@@ -37,96 +38,98 @@ def get_notes(notes_to_parse):
 
     return {"start":start, "pitch":notes, "dur":durations}
 
-def midi2image(midi_path):
+def midi2binary(midi_path):
+    # returns a dictionionary with keys = part numbers, value = list with np binary matrices of the image.  
+    return midi2image(midi_path, True)
+
+def midi2image(midi_path, binary = False):
+    # Cover your eyes, children. 
+    # Warning: function returns nothing (None), but catch the return anyway
     mid = converter.parse(midi_path)
+    binary_matrix_list = {} #parts
+    parts = mid.recurse().getElementsByClass('Part')
 
-    instruments = instrument.partitionByInstrument(mid)
-
-    data = {}
-
-    try:
-        i=0
-        for instrument_i in instruments.parts:
-            notes_to_parse = instrument_i.recurse()
-
-            if instrument_i.partName is None:
-                data["instrument_{}".format(i)] = get_notes(notes_to_parse)
-                i+=1
-            else:
-                data[instrument_i.partName] = get_notes(notes_to_parse)
-
-    except:
-        notes_to_parse = mid.flat.notes
-        data["instrument_0".format(i)] = get_notes(notes_to_parse)
-
-    resolution = 0.25
-
-    for instrument_name, values in data.items():
-        # https://en.wikipedia.org/wiki/Scientific_pitch_notation#Similar_systems
-        upperBoundNote = 127 # height of image
-        lowerBoundNote = 21
-        maxSongLength = 128 # length of image
-
-        # padding TODO
-        padding = False
-
-        if maxSongLength%2 != 0:
-            print("Warning: padding selected but length not even, change maxSongLength = {} to be an even number".format(maxSongLength))
+    for partidx, midpart in enumerate(parts):
         
-        padding_size = int((maxSongLength - (upperBoundNote - lowerBoundNote)) / 2)
-        print(padding_size)
+        data = {}
 
-        # calculate the amount of images required to display the full song
-        images = int((len(values["pitch"]) // maxSongLength)) + 5
-        print("{} {} {}".format(len(values["pitch"]), maxSongLength, images))
+        notes_to_parse = midpart.recurse()
+        data["part_{}".format(partidx)] = get_notes(notes_to_parse)
+      
+        resolution = 0.25
 
-        index = 0
-        prev_index = 0
-        repetitions = 0
+        for part_name, values in data.items():
+            # https://en.wikipedia.org/wiki/Scientific_pitch_notation#Similar_systems
+            upperBoundNote = 127 # height of image
+            lowerBoundNote = 21
+            maxSongLength = 128 # length of image
 
-        # TODO: number of images generated is too large, seems to have something to do with that the len(values[pitch]) is larger than supposed (?).
-        # so the basecase is never reached, and the prev_index is not increased anymore after a while, meaning empty loops are performed creating empty images
+            binary_matrix_list[part_name] = [] #list of entries in part (each of which will contrain matrices)
 
-        while repetitions < images:
-            if prev_index >= len(values["pitch"]):
-                break
+            # padding 
+            padding = False
 
-            # Image matrix
-            if padding:
-                matrix = np.zeros((maxSongLength,maxSongLength))
-            else :
-                matrix = np.zeros((upperBoundNote-lowerBoundNote,maxSongLength))
+            if maxSongLength%2 != 0:
+                print("Warning: padding selected but length not even, change maxSongLength = {} to be an even number".format(maxSongLength))
+            
+            padding_size = int((maxSongLength - (upperBoundNote - lowerBoundNote)) / 2)
 
-            pitchs = values["pitch"]
-            durs = values["dur"]
-            starts = values["start"]
+            # calculate the amount of images required to display the full song
+            images = math.ceil(((max(values["start"]) + max(values["dur"])) * 4) / maxSongLength)
 
-            # From where we left off to the end
-            for i in range(prev_index,len(pitchs)):
-                pitch = pitchs[i]
+            index = 0
+            prev_index = 0
+            repetitions = 0
 
-                dur = int(durs[i]/resolution)
-                start = int(starts[i]/resolution)
-
-                # if we're not at the end of the image
-                if dur+start - index*maxSongLength < maxSongLength:
-                    # loop over something*
-                    for j in range(start,start+dur):
-                        if j - index*maxSongLength >= 0:
-                            # with padding: add some padding pixels to the bottom (already added to the top by the matrix size) to create a square image
-                            if padding:
-                                matrix[pitch-lowerBoundNote + padding_size,j - index*maxSongLength] = 255
-                            else:
-                                matrix[pitch-lowerBoundNote,j - index*maxSongLength] = 255
-                # when we are, break
-                else:
-                    prev_index = i
+            while repetitions < images:
+                if prev_index >= len(values["pitch"]):
                     break
 
-            imwrite(midi_path.split("/")[-1].replace(".mid",f"_{instrument_name}_{index}.png"),matrix)
-            index += 1
-            repetitions+=1
+                # Image matrix
+                if padding:
+                    matrix = np.zeros((maxSongLength,maxSongLength))
+                else :
+                    matrix = np.zeros((upperBoundNote-lowerBoundNote,maxSongLength))
+
+                pitchs = values["pitch"]
+                durs = values["dur"]
+                starts = values["start"]
+
+                # From where we left off to the end
+                for i in range(prev_index,len(pitchs)):
+                    pitch = pitchs[i]
+
+                    dur = int(durs[i]/resolution)
+                    start = int(starts[i]/resolution)
+
+                    # if we're not at the end of the image
+                    if dur+start - index*maxSongLength < maxSongLength:
+                        # loop over something*
+                        for j in range(start,start+dur):
+                            if j - index*maxSongLength >= 0:
+                                # with padding: add some padding pixels to the bottom (already added to the top by the matrix size) to create a square image
+                                if padding:
+                                    matrix[pitch-lowerBoundNote + padding_size,j - index*maxSongLength] = 255
+                                else:
+                                    matrix[pitch-lowerBoundNote,j - index*maxSongLength] = 255
+                    # when we are, break
+                    else:
+                        prev_index = i
+                        break
+                if not (matrix == np.zeros(matrix.shape)).all() and not binary:
+                    imwrite(midi_path.split("/")[-1].replace(".mid",f"_{part_name}_{index}.png"),matrix)
+                elif binary:
+                    binary_matrix_list[part_name].append(matrix > 0)
+                else:
+                    print("Empty generated image discarded!")
+                index += 1
+                repetitions+=1
+    if binary:
+        return binary_matrix_list
+    else :
+        # Abe may refractor if he wants, we dont want 
+        return None
 
 import sys
 midi_path = sys.argv[1]
-midi2image(midi_path)
+matrix = midi2binary(midi_path)
