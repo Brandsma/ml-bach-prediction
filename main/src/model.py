d0a5efaec5c1e4d2cbec54d0dfa5abde4de14252
Brandsma <a.brandsma@protonmail.com>
2021-02-09 19:13:44 +0100

    Test higher resnets

--- main/src/model.py
+++ main/src/model.py
@@ -31,8 +31,8 @@ def create_model(config, image_shape):
     # TODO: Look at documentation of pixelCNN
     dist = tfd.PixelCNN(
         image_shape=image_shape,
-        num_resnet=1,
-        num_hierarchies=2,
+        num_resnet=6,
+        num_hierarchies=4,
         num_filters=32,
         num_logistic_mix=5,
         dropout_p=0.3,
